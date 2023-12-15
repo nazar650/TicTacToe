8beb777680d67f1bef875b48e26847ab941a3545
назар Матвеев <nazarmatveev0@gmail.com>
2023-12-15 18:13:31 +0200

    add Input

--- Project4/FileName.cpp
+++ Project4/FileName.cpp
@@ -2,3 +2,29 @@
 #include <windows.h>
 using namespace std;
 #define SIZE 3;
+void Input(char** num1, int& x, int& y) {
+
+
+    char key = _getwch();
+    if ((key == 'w' || key == 'W') && y > 0) {
+        y--;
+    }
+    if ((key == 's' || key == 'S') && y < 2) {
+        y++;
+    }
+    if ((key == 'a' || key == 'A') && x > 0) {
+        x--;
+    }
+    if ((key == 'd' || key == 'D') && x < 2) {
+        x++;
+    }
+    if (key == '\r' && num1[y][x] != 'o') {
+
+        num1[y][x] = 'x';
+    }
+
+    if (key == ' ' && num1[y][x] != 'x') {
+        num1[y][x] = 'o';
+
+    }
+}
