bb52cfbfe3e1dd500940b6972f4e519ff6875cec
назар Матвеев <nazarmatveev0@gmail.com>
2023-12-15 19:34:24 +0200

    add Draw

--- Project4/FileName.cpp
+++ Project4/FileName.cpp
@@ -28,3 +28,22 @@ void Input(char** num1, int& x, int& y) {
 
     }
 }
+void Draw(char** num1, int  x, int y) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << "+---+---+---+" << endl;
+        for (int j = 0; j < SIZE; j++)
+        {
+            cout << "|" << " ";
+            if (x == j && y == i) {
+                SetConsoleTextAttribute(h, (2 << 4) | 8);
+            }
+            cout << (num1[i][j]);
+            SetConsoleTextAttribute(h, (0 << 4) | 8);
+            cout << " ";
+        }
+        cout << "|" << endl;
+    }
+    cout << "+---+---+---+" << endl;
+}
